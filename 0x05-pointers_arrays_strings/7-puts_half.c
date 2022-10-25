cbb16c0899a37dd69c1a53f8134cf4e39abcba6e
Paul Ologunja <ologunjapaul@gmail.com>
2022-10-24 20:32:19 -0700

    Array copy

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string input
+ * Return: Second half of given string
+ */
+void puts_half(char *str)
+{
+	int c, n, length_of_the_string;
+
+	length_of_the_string = 0;
+
+	for (c = 0; str[c] != '\0'; c++)
+		length_of_the_string++;
+
+	n = (length_of_the_string / 2);
+
+	if (length_of_the_string % 2 == 1)
+		n = ((length_of_the_string - 1) / 2);
+
+	for (c = n; str[c] != '\0'; c++)
+		_putchar(str[c]);
+
+	_putchar('\n');
+}
